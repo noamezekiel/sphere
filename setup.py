0e927a85a70701b30e7b5bab4ba11f48cf2698ac
Noam Ezekiel <noamezekiel@gmail.com>
2020-04-09 15:32:28 +0300

    fixing setup

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup, find_packages
 
 
+with open('requirements.txt') as fp:
+    install_requires = fp.read().splitlines()
+
+
 setup(
     name='sphere',
     version='0.1.0',
     author='Noam Ezekiel',
     description='A Brain Computer Interface system',
     packages=find_packages(),
-    install_requires=['click', 'flask', 'Pillow', 'protobuf3', 'requests'],
+    install_requires=install_requires,
     tests_require=['pytest', 'pytest-cov'],
 )
