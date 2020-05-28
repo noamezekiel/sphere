1ed52aea96df9a06ecd1bbd2749fadc9fa2f9701
Noam Ezekiel <noamezekiel@gmail.com>
2020-05-28 14:02:48 +0300

    added docker-compose to install

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,6 +5,8 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 
 function main {
+	sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
     python -m virtualenv .env --prompt "[sphere] "
     find .env -name site-packages -exec bash -c 'echo "../../../../" > {}/self.pth' \;
     .env/bin/pip install -U pip
